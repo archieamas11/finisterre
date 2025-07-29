c59d5630791d48fe9dc327751e7a9a74a8719154
archieamas11 <archiealbarico69@gmail.com>
2025-07-29 15:10:53 +0800

    ✨ feat(auth): Enhance Forgot and Reset Password UI
    - Update layout for better responsiveness and user experience
    - Improve text clarity for instructions
    - Add a 'Back' button for easier navigation

--- src/auth/page.tsx
+++ src/auth/page.tsx
@@ -8,12 +8,14 @@ export default function LoginV2() {
     <>
       <div className="mx-auto flex w-full flex-col justify-center space-y-8 sm:w-[350px]">
         <div className="space-y-2 text-center">
-          <Link
-            to="/"
-            className="flex items-center justify-center lg:hidden"
-          >
-            <MapPin className="h-20 w-20" />
-          </Link>
+          <div className="flex justify-center">
+            <Link
+              to="/"
+              className="flex items-center justify-center lg:hidden border border-primary/10 bg-primary/10 rounded-full p-3 w-16 h-16 aspect-square mb-5"
+            >
+              <MapPin className="h-20 w-20" />
+            </Link>
+          </div>
           <h1 className="text-3xl font-medium">Login to your account</h1>
           <p className="text-muted-foreground text-sm">Please enter your details to login.</p>
         </div>
@@ -22,7 +24,7 @@ export default function LoginV2() {
         </div>
       </div>
 
-      <div className="absolute bottom-5 flex w-full justify-between px-10">
+      <div className="absolute bottom-5 flex w-full justify-center px-10">
         <div className="text-sm">{APP_CONFIG.copyright}</div>
       </div>
     </>
