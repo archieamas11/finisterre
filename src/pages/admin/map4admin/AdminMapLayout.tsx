40c4e9fe9544e422e84db42eba0b56e2097cff0d
archieamas11 <archiealbarico69@gmail.com>
2025-08-22 20:04:41 +0800

    🔧 refactor(AdminMapLayout): Import useCallback for optimization
    - Enhance performance by importing useCallback
    - Prepare for potential memoization of components

--- src/pages/admin/map4admin/AdminMapLayout.tsx
+++ src/pages/admin/map4admin/AdminMapLayout.tsx
@@ -4,7 +4,7 @@ import L from 'leaflet'
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
 import iconUrl from 'leaflet/dist/images/marker-icon.png'
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
-import { useRef, useState, useEffect, Suspense, lazy } from 'react'
+import { useRef, useState, useEffect, Suspense, lazy, useCallback } from 'react'
 import { MapContainer, TileLayer, Popup, GeoJSON } from 'react-leaflet'
 import MarkerClusterGroup from 'react-leaflet-markercluster'
 
