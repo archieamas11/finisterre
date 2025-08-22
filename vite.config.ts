ee1fc41a5e147daac9d63a8f2ccdd3ec0f0749d2
archieamas11 <archiealbarico69@gmail.com>
2025-08-22 20:00:30 +0800

    🔧 refactor(vite): Remove unused plugins and auto-imports
    - Deleted the AutoImport plugin and its configuration
    - Removed the removeConsole plugin from the Vite configuration
    - Cleaned up the auto-imports.d.ts file as it is no longer needed

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,11 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
-import removeConsole from 'vite-plugin-remove-console'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    removeConsole(),
-    AutoImport({
-      imports: ['react', 'react-router-dom'],
-      dts: 'src/auto-imports.d.ts'
-    })
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     target: 'esnext'
   },
